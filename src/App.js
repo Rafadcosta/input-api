557e044fea78a1e4decacd7b1d96141968325b24
Rafaela Duque <rafaela@apus.info>
2021-01-05 14:22:29 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text'
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email'
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password'
+  },
+  {
+    id: 'cep',
+    label: 'cep',
+    type: 'text'
+  },
+  {
+    id: 'rua',
+    label: 'rua',
+    type: 'text'
+  },
+  {
+    id: 'numero',
+    label: 'numero',
+    type: 'text'
+  },
+  {
+    id: 'bairro',
+    label: 'bairro',
+    type: 'text'
+  },
+  {
+    id: 'cidade',
+    label: 'cidade',
+    type: 'text'
+  },
+  {
+    id: 'estado',
+    label: 'estado',
+    type: 'text'
+  },
+]
+
+
+const App = () => {
+
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
+
+  const [response, setResponse] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(form),
+    }).then(response => {
+      setResponse(response);
+    })
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form onSubmit={handleSubmit}>
+
+        {formFields.map(field => (
+          <div key={field.id}>
+            <label htmlFor={field.id}>{field.label}</label> <br/>
+            <input
+              type={field.type}
+              id={field.id}
+              value={form[field.id]}
+              onChange={handleChange}
+            /> <br/> <br/>
+          </div>
+        ))}
+        {response && response.ok && <p>Formulário enviado</p>}
+        <button>Enviar</button>
+      </form>
+
     </div>
   );
 }
